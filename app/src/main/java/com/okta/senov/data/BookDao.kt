663c0437833fe5501a0e576d9f56f45b7cd1f687
oktapianir <ramadhanioktapiani@gmail.com>
2024-12-03 11:21:49 +0700

    data commit

---
+++ app/src/main/java/com/okta/senov/data/BookDao.kt
@@ -0,0 +1,25 @@
+package com.okta.senov.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.okta.senov.model.Book
+
+@Dao
+interface BookDao {
+
+    @Query("SELECT * FROM books")
+    suspend fun getAllBooks(): List<Book>
+
+    @Query("DELETE FROM books")
+    suspend fun deleteAllBooks()
+
+    @Update
+    suspend fun update(book: Book)
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBooks(books: List<Book>)
+}
---
+++ app/src/main/java/com/okta/senov/data/BookDatabase.kt
@@ -0,0 +1,29 @@
+package com.okta.senov.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.okta.senov.model.Book
+
+@Database(entities = [Book::class], version = 1, exportSchema = false)
+abstract class BookDatabase : RoomDatabase() {
+    abstract fun bookDao(): BookDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: BookDatabase? = null
+
+        fun getDatabase(context: Context): BookDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    BookDatabase::class.java,
+                    "book_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
