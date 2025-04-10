2ffc216ac0ecbfa78e759394c648007aa2b300c0
oktapiani <ramadhanioktapiani@gmail.com>
2025-04-10 07:51:54 +0700

    docs: add generate comment on each function

--- app/src/main/java/com/okta/senov/extensions/ViewExtensions.kt
+++ app/src/main/java/com/okta/senov/extensions/ViewExtensions.kt
@@ -3,7 +3,9 @@ package com.okta.senov.extensions
 import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-
+//// Extension function untuk View agar bisa langsung mendapatkan NavController
 fun View.findNavController(): NavController {
+    // Menggunakan Navigation.findNavController dari AndroidX
+    // untuk mendapatkan NavController yang terkait dengan View ini
     return Navigation.findNavController(this)
 }
